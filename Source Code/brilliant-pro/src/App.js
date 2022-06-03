8ae851fd60e16c914f5c6e38728ba451af40d83a
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-06-03 10:00:46 +0500

    Updates

--- Source Code/brilliant-pro/src/App.js
+++ Source Code/brilliant-pro/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Homepage from './Components/Common Interface/Homepage/Homepage';
 import {Routes, Route, BrowserRouter as Router, useLocation} from 'react-router-dom';
+import Navbar from './Components/Common Interface/Navbar/Navbar';
+import {useLayoutEffect } from 'react'
 
-function App() {
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+};
+
+const App = () => {
   return (
     <Router>
-      <Homepage />
+      <Wrapper>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Homepage/>} />   
+        </Routes>
+      </Wrapper>
     </Router>   
   );
 }
--- Source Code/brilliant-pro/src/Components/Common Interface/Homepage/Homepage.js
+++ Source Code/brilliant-pro/src/Components/Common Interface/Homepage/Homepage.js
@@ -4,30 +4,19 @@ import './Homepage.css';
 function Homepage() {
   return (
     <>
-    
       <div className='home-section'>
         <div className='home-img-wrapper'>
-            <img src='./homepage-extras/bg.jpg' alt='Image not loaded' />
-            <div className='container'>
-                <div className='row home-row' style={{display: 'flex'}}>
-                <div className='col'>
-                    <div className='home-text-wrapper' >
-                    <h1 className='home-heading dark'>
-                        Creating <span>Smart Solutions</span>
-                    </h1>
-                    <p className= 'home-subtitle dark'>
-                        At Think Vision, we provide real-time monitoring and threat alerts using deep learning and video analytics.
-                    </p>
-                    </div>
-                </div>
-                
-                </div>
+            <img src='./homepage-extras/bg.jpg' alt='Image not loaded' className='buuu' />
+            <div className='home-text-wrapper' >
+              <h1 className='home-heading dark'>
+                  Creating <span>Smart Solutions</span>
+              </h1>
+              <p className= 'home-subtitle dark'>
+                  At Think Vision, we provide real-time monitoring and threat alerts using deep learning and video analytics.
+              </p>
             </div>
         </div>
       </div>
-
-      
-
     </>
   );
 }
